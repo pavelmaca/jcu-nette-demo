6ab351cbc071954d2149d3cb1821d15ce4aea10f
Pavel Máca <maca.pavel@gmail.com>
2017-05-14 11:56:57 +0200

    prázdný ArticlePresenter a odkaz na detail článku

---
+++ app/presenters/ArticlePresenter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Pavel
+ * Date: 14.5.2017
+ * Time: 11:55
+ */
+
+namespace App\Presenters;
+
+
+class ArticlePresenter extends BasePresenter
+{
+
+
+
+}
